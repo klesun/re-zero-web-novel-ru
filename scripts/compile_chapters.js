f0a3a4d2acfd12b8cbbe043a1628d2cfcaec47da
Artur Klesun <arturklesun@gmail.com>
2024-05-12 22:52:51 +0300

    Add pitch bending for different speakers

--- scripts/compile_chapters.js
+++ scripts/compile_chapters.js
@@ -1,13 +1,11 @@
 import fs from "fs/promises";
-import util from "util";
-import { exec } from "child_process";
 import ffmpeg from 'fluent-ffmpeg';
 import { promisify } from "util";
 import NodeID3, { TagConstants } from 'node-id3';
 
-const ffprobe = promisify(ffmpeg.ffprobe);
 const writeTags = promisify(NodeID3.write);
 
+/** @param {EventEmitter} eventEmitter */
 function once(eventEmitter) {
     return new Promise((resolve, reject) => {
         eventEmitter
@@ -16,102 +14,143 @@ function once(eventEmitter) {
     });
 }
 
-const execAsync = command => new Promise((resolve, reject) => exec(command, (error, stdout, stderr) => {
-    if (error) {
-        reject(error);
-    } else {
-        resolve([stdout, stderr]);
-    }
-}));
-
 const exit = process.exit;
 process.exit = (status) => { throw new Error("Fucking lib tried to process.exit " + status); };
 
+/** @param {Response} rs */
+async function makeImageTag(rs) {
+    const buff = await rs.arrayBuffer();
+    const mime = rs.headers.get("Content-Type");
+    return {
+        mime,
+        imageBuffer: Buffer.from(buff),
+        type: {
+           id: TagConstants.AttachedPicture.PictureType.FRONT_COVER,
+        },
+        description: "Chapter Illustration",
+    };
+}
 
-async function main() {
-    const chapters = JSON.parse(await fs.readFile("./chapters.json", "utf8"));
+const whenAlbumCover = fetch("https://witchculttranslation.com/wp-content/uploads/2019/01/DtAgHD1VAAA9XEP.jpg_large.jpg")
+    .then(makeImageTag);
 
-    let lastEndTime = 0;
+const speakers = JSON.parse(await fs.readFile("./speakers.json", "utf8"));
+const nameToSpeaker = Object.fromEntries(speakers.map(s => [s.name, s]));
 
-    const speeches = [];
-    for (const p of chapters[0].paragraphs) {
-        if (p.text.trim() === "") {
-            continue;
-        }
-        const speechPath = p.text.match(/^(\s*※\s*)+$/) || p.text.match(/^(\s*\*\s*)+$/)
-            ? "special_speeches/few_moments_later.oga"
-            : `speeches/${p.id}.oga`;
-        speeches.push({
-            speechPath: speechPath,
-            startTime: lastEndTime,
-            text: p.text,
-        });
-        const metadata = await ffprobe(speechPath);
-        lastEndTime += metadata.format.duration;
-        console.log("ololo " + p.id + " " + lastEndTime);
-    }
-    const listFileContent = speeches
-        .map(s => `file '../${s.speechPath}'`)
-        .join("\n");
-    await fs.writeFile("./tmp/list.txt", listFileContent);
-
-    let concatenation = ffmpeg("tmp/list.txt")
-        .inputFormat("concat")
-        .inputOptions(['-safe 0'])
-        .outputOptions(["-c copy"])
-        .on('start', (ffmpegCommand) => console.log(ffmpegCommand))
-        .output("compiled_chap1.oga");
-    concatenation.run();
-    await once(concatenation);
-    console.log("zhopa ffmpeg concat end");
-
-    const mp3Conversion = ffmpeg('compiled_chap1.oga')
-        .output('compiled_chap1_clean.mp3');
-    mp3Conversion.run();
-    await once(mp3Conversion);
-
-    await writeTags({
+/** @return {Tags} */
+async function getTags(chapter, trackNumber) {
+    return {
         album: "Re:Zero Арка 5 Звёзды что Вершат Историю",
         artist: "ru-RU-DmitryNeural",
         artistUrl: "https://github.com/klesun/re-zero-web-novel-ru",
         audioSourceUrl: "https://github.com/klesun/re-zero-web-novel-ru",
         comment: "Generated using fan translations and Microsoft Text-to-Speech services",
         composer: "klesun",
-        textWriter: "Эльрат",
-        chapter: 1,
-        trackNumber: 1,
-        title: chapters[0].title,
+        textWriter: chapter.translator,
+        chapter: chapter.chapter,
+        trackNumber: trackNumber,
+        title: chapter.title,
         copyright: "2024 klesun CC BY 4.0 DEED Attribution 4.0 International",
         copyrightUrl: "https://creativecommons.org/licenses/by/4.0/",
         genre: "(183)Audiobook",
         language: "rus",
-        originalTitle: "始まりはいつも来訪者から",
+        originalTitle: trackNumber === 1 ? "始まりはいつも来訪者から" : undefined,
         originalTextwriter: "長月達平",
-        originalReleaseTime: "2014-09-17",
-        image: await fetch("https://witchculttranslation.com/wp-content/uploads/2018/11/a5c1.png").then(async rs => {
-            const buff = await rs.arrayBuffer();
-            const mime = rs.headers.get("Content-Type");
-            console.log("image " + mime + " " + buff.byteLength);
-            return {
-                mime,
-                imageBuffer: Buffer.from(buff),
-                type: {
-                   id: TagConstants.AttachedPicture.PictureType.FRONT_COVER,
-                },
-                description: "Chapter Illustration",
-            };
-        }),
+        originalReleaseTime: trackNumber === 1 ? "2014-09-17" : undefined,
+        image: trackNumber === 1
+            ? await fetch("https://witchculttranslation.com/wp-content/uploads/2018/11/a5c1.png").then(makeImageTag)
+            : await whenAlbumCover,
         synchronisedLyrics: [{
             language: "rus",
             timeStampFormat: TagConstants.TimeStampFormat.MILLISECONDS,
             contentType: TagConstants.SynchronisedLyrics.ContentType.LYRICS,
-            shortText: speeches.length + " speeches follow. SYLT frames are supported by PowerAmp and MusicBee players on the moment of writing. If you see this, then your player is trying to incorrectly interpret SYLT id3v2 tag as .lrc and it should be ashamed for adding to the confusion of the SYLT format.",
-            synchronisedText: speeches.map(speech => ({
-                text: speech.text,
+            shortText: chapter.paragraphs.length + " speeches follow. SYLT frames are supported by PowerAmp and MusicBee players on the moment of writing. If you see this, then your player is trying to incorrectly interpret SYLT id3v2 tag as .lrc and it should be ashamed for adding to the confusion of the SYLT format.",
+            synchronisedText: chapter.paragraphs.map(speech => ({
+                text: speech.speaker === "Narrator" ? speech.text : "\"" + speech.text + "\" - " + speech.speaker,
                 timeStamp: Math.floor(speech.startTime * 1000),
             })),
         }],
-    }, "compiled_chap1_clean.mp3");
+    };
+}
+
+function hashCode(str) {
+  var hash = 0,
+    i, chr;
+  if (str.length === 0) return hash;
+  for (i = 0; i < str.length; i++) {
+    chr = str.charCodeAt(i);
+    hash = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+}
+
+async function compileChapter(chapter, trackNumber) {
+    let lastEndTime = 0;
+    for (const p of chapter.paragraphs) {
+        let speakerData = nameToSpeaker[p.speaker];
+        if (speakerData?.sameAs) {
+            speakerData = nameToSpeaker[speakerData.sameAs];
+        }
+        let pitchFactor = speakerData?.pitchFactor ?? 1;
+        if (speakerData && !speakerData.pitchFactor && speakerData.name !== "Narrator") {
+            const hashCodeValue = Math.abs(hashCode(speakerData.name));
+            const extra = Math.abs(hashCodeValue) / (2 ** 31);
+            pitchFactor = 0.85 + extra * 0.3;
+        }
+        if (pitchFactor !== 1) {
+            const pitchBentPath = "speeches_pitch_bent/" + p.id + ".oga";
+
+            const newFrequency = pitchFactor * 48000;
+            const bending = ffmpeg(p.speechPath)
+                .on('start', (ffmpegCommand) => console.log(ffmpegCommand))
+                .audioFilter({
+                    filter: "asetrate",
+                    options: Math.floor(newFrequency),
+                })
+                .outputOptions(["-c libopus"])
+                .output(pitchBentPath);
+            bending.run();
+            await once(bending);
+
+            p.speechPath = pitchBentPath;
+        }
+        p.startTime = lastEndTime;
+        lastEndTime += p.duration / pitchFactor;
+    }
+    const listFileContent = chapter.paragraphs
+        .map(s => `file '../${s.speechPath}'`)
+        .join("\n");
+    await fs.writeFile("./unv/list.txt", listFileContent);
+
+    const concatFileName = "unv/tmp_concat/compiled_chap_" + chapter.chapter + ".oga";
+    const outputFileName = "unv/output/compiled_chap_" + chapter.chapter + ".mp3";
+
+    let concatenation = ffmpeg("./unv/list.txt")
+        .inputFormat("concat")
+        .inputOptions(['-safe 0'])
+        .outputOptions(["-c copy"])
+        .on('start', (ffmpegCommand) => console.log(ffmpegCommand))
+        .output(concatFileName);
+
+    concatenation.run();
+    await once(concatenation);
+
+    const mp3Conversion = ffmpeg(concatFileName)
+        .output(outputFileName);
+    mp3Conversion.run();
+    await once(mp3Conversion);
+
+    const tags = await getTags(chapter, trackNumber);
+    await writeTags(tags, outputFileName);
+}
+
+async function main() {
+    const chapters = JSON.parse(await fs.readFile("./chapters.json", "utf8"));
+    let trackNumber = 0;
+    for (const chapter of chapters) {
+        await compileChapter(chapter, ++trackNumber);
+    }
 }
 
 main().then(
@@ -121,22 +160,3 @@ main().then(
         exit(1);
     }
 );
-
-// const concatenation = ffmpeg()
-//     .input('./tmp/list.txt')
-//     .inputOptions(['-f concat', '-safe 0'])
-//     .outputOptions('-c copy')
-//     .save('compiled_chap1.oga');
-// console.log(concatenation);
-// await new Promise(
-//     (resolve, reject) => concatenation  // Output file
-//         .on('error', reject)
-//         .on('end', resolve)
-// );
-//
-// let stdout, stderr;
-// [stdout, stderr] = await execAsync("C:\\m\\ffmpeg\\bin\\ffmpeg.exe -f concat -safe 0 -i .\\tmp\\list.txt -c copy compiled_chap1.oga");
-// console.log("STDOUT");
-// console.log(stdout);
-// console.log("STDERR");
-// console.log(stderr);
